f6a421a4dd993dbd32200786a462d02f0cd16aab
Spec <specmedia@icloud.com>
2016-06-17 00:44:59 +0300

    level14-08-09

--- src/com/javarush/test/level14/lesson08/home06/Solution.java
+++ src/com/javarush/test/level14/lesson08/home06/Solution.java
@@ -17,11 +17,38 @@ package com.javarush.test.level14.lesson08.home06;
 8.2. Вывести на экран movie.getClass().getSimpleName().
 */
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Solution
 {
     public static void main(String[] args) throws Exception
     {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String key = null;
+        while (true){
+            key = reader.readLine();
+            if (key.equalsIgnoreCase("cartoon")){
+                Movie movie = MovieFactory.getMovie(key);
+                System.out.println(movie.getClass().getSimpleName());
+            }
+            else if (key.equalsIgnoreCase("thriller")){
+                Movie movie = MovieFactory.getMovie(key);
+                System.out.println(movie.getClass().getSimpleName());
+            }
+            else if (key.equalsIgnoreCase("soapOpera")){
+                Movie movie = MovieFactory.getMovie(key);
+                System.out.println(movie.getClass().getSimpleName());
+            }
+            else break;
+        }
+
+
         //ввести с консоли несколько ключей (строк), пункт 7
+
+
 
         /*
 8 Создать переменную movie класса Movie и для каждой введенной строки(ключа):
@@ -43,6 +70,15 @@ public class Solution
             {
                 movie = new SoapOpera();
             }
+            if ("cartoon".equals(key))
+            {
+                movie = new Cartoon();
+            }
+            if ("thriller".equals(key))
+            {
+                movie = new Thriller();
+            }
+
 
             //напишите тут ваш код, пункты 5,6
 
@@ -56,6 +92,12 @@ public class Solution
 
     static class SoapOpera extends Movie
     {
+    }
+    static class Cartoon extends Movie{
+
+    }
+    static class Thriller extends Movie{
+
     }
 
     //Напишите тут ваши классы, пункт 3
